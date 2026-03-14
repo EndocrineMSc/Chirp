09d6914d221874a22af65f7f9b78d7bf29d3bde7
Sebastian Schulz <s.schulz@oput.de>
2026-03-14 08:09:12 +0100

    chore: Setup DeviceConfiguration enum helper

---
+++ core/presentation/src/commonMain/kotlin/com/endocrine/core/presentation/util/DeviceConfiguration.kt
@@ -0,0 +1,50 @@
+package com.endocrine.core.presentation.util
+
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.runtime.Composable
+import androidx.window.core.layout.WindowSizeClass
+import androidx.window.core.layout.WindowSizeClass.Companion.HEIGHT_DP_EXPANDED_LOWER_BOUND
+import androidx.window.core.layout.WindowSizeClass.Companion.HEIGHT_DP_MEDIUM_LOWER_BOUND
+import androidx.window.core.layout.WindowSizeClass.Companion.WIDTH_DP_EXPANDED_LOWER_BOUND
+import androidx.window.core.layout.WindowSizeClass.Companion.WIDTH_DP_MEDIUM_LOWER_BOUND
+
+/**
+ * Returns the current [DeviceConfiguration] based on the current window adaptive information.
+ */
+@Composable
+fun currentDeviceConfiguration(): DeviceConfiguration {
+    val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+    return DeviceConfiguration.fromWindowSizeClass(windowSizeClass)
+}
+
+/**
+ * Represents the device configuration based on screen orientation and device type.
+ */
+enum class DeviceConfiguration {
+    MOBILE_PORTRAIT,
+    MOBILE_LANDSCAPE,
+    TABLET_PORTRAIT,
+    TABLET_LANDSCAPE,
+    DESKTOP;
+
+    companion object {
+        /**
+         * Maps a [WindowSizeClass] to a [DeviceConfiguration] based on width and height thresholds.
+         */
+        fun fromWindowSizeClass(windowSizeClass: WindowSizeClass): DeviceConfiguration {
+            return with(windowSizeClass) {
+                when {
+                    minWidthDp < WIDTH_DP_MEDIUM_LOWER_BOUND &&
+                            minHeightDp >= HEIGHT_DP_MEDIUM_LOWER_BOUND -> MOBILE_PORTRAIT
+                    minWidthDp >= WIDTH_DP_EXPANDED_LOWER_BOUND &&
+                            minHeightDp < HEIGHT_DP_MEDIUM_LOWER_BOUND -> MOBILE_LANDSCAPE
+                    minWidthDp in WIDTH_DP_MEDIUM_LOWER_BOUND..WIDTH_DP_EXPANDED_LOWER_BOUND &&
+                            minHeightDp >= HEIGHT_DP_EXPANDED_LOWER_BOUND -> TABLET_PORTRAIT
+                    minWidthDp >= WIDTH_DP_EXPANDED_LOWER_BOUND &&
+                            minHeightDp in HEIGHT_DP_MEDIUM_LOWER_BOUND..HEIGHT_DP_EXPANDED_LOWER_BOUND -> TABLET_LANDSCAPE
+                    else -> DESKTOP
+                }
+            }
+        }
+    }
+}
