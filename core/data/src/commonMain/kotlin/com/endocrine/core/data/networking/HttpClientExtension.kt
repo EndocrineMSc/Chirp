3a11b001f2fc0196cb22c29d4917bb18c976872d
Sebastian Schulz <s.schulz@oput.de>
2026-03-14 07:04:55 +0100

    chore: Add HttpClient CRUD wrappers

--- core/data/src/commonMain/kotlin/com/endocrine/core/data/networking/HttpClientExtension.kt
+++ core/data/src/commonMain/kotlin/com/endocrine/core/data/networking/HttpClientExtension.kt
@@ -2,36 +2,118 @@ package com.endocrine.core.data.networking
 
 import com.endocrine.core.domain.Result
 import com.endocrine.core.domain.util.DataError
+import io.ktor.client.HttpClient
 import io.ktor.client.call.NoTransformationFoundException
 import io.ktor.client.call.body
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.delete
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.request.put
+import io.ktor.client.request.setBody
+import io.ktor.client.request.url
 import io.ktor.client.statement.HttpResponse
 
 /**
- * Executes a network call in a platform-safe way, handling platform-specific network exceptions
- * and converting them into [DataError.Remote] failures.
+ * Performs a POST request with the specified [route], [queryParams], and [body],
+ * returning a [Result] to simplify error handling and response parsing.
  *
- * @param execute A suspending lambda that performs the actual network request and returns an [HttpResponse].
- * @param handleResponse A suspending lambda that processes the successful [HttpResponse] and maps it to a [Result].
- * @return A [Result] containing either the success value of type [T] or a [DataError.Remote].
+ * @param route The relative or absolute URL for the request.
+ * @param queryParams A map of key-value pairs to be appended to the URL.
+ * @param body The data to be sent in the request body, which will be serialized.
+ * @param builder A configuration block for the [HttpRequestBuilder], allowing custom headers or settings.
  */
-expect suspend fun <T> platformSafeCall(
-    execute: suspend () -> HttpResponse,
-    handleResponse: suspend (HttpResponse) -> Result<T, DataError.Remote>
-): Result<T, DataError.Remote>
+suspend inline fun <reified Request, reified Response : Any> HttpClient.post(
+    route: String,
+    queryParams: Map<String, Any> = mapOf(),
+    body: Request,
+    crossinline builder: HttpRequestBuilder.() -> Unit = {}
+): Result<Response, DataError.Remote> {
+    return safeCall {
+        post {
+            url(constructRoute(route))
+            queryParams.forEach { (key, value) ->
+                parameter(key, value)
+            }
+            setBody(body)
+            builder()
+        }
+    }
+}
 
 /**
- * A convenience wrapper around [platformSafeCall] that uses [responseToResult] to handle the response.
+ * Performs a GET request with the specified [route] and [queryParams],
+ * returning a [Result] to simplify error handling and response parsing.
  *
- * @param execute A suspending lambda that performs the actual network request and returns an [HttpResponse].
- * @return A [Result] containing either the success value of type [T] or a [DataError.Remote].
+ * @param route The relative or absolute URL for the request.
+ * @param queryParams A map of key-value pairs to be appended to the URL.
+ * @param builder A configuration block for the [HttpRequestBuilder], allowing custom headers or settings.
  */
-suspend inline fun <reified T> safeCall(
-    noinline execute: suspend() -> HttpResponse
-): Result<T, DataError.Remote> {
-    return platformSafeCall(
-        execute = execute
-    ) { response ->
-        responseToResult(response)
+suspend inline fun <reified Request, reified Response : Any> HttpClient.get(
+    route: String,
+    queryParams: Map<String, Any> = mapOf(),
+    crossinline builder: HttpRequestBuilder.() -> Unit = {}
+): Result<Response, DataError.Remote> {
+    return safeCall {
+        get {
+            url(constructRoute(route))
+            queryParams.forEach { (key, value) ->
+                parameter(key, value)
+            }
+            builder()
+        }
+    }
+}
+
+/**
+ * Performs a PUT request with the specified [route], [queryParams], and [body],
+ * returning a [Result] to simplify error handling and response parsing.
+ *
+ * @param route The relative or absolute URL for the request.
+ * @param queryParams A map of key-value pairs to be appended to the URL.
+ * @param body The data to be sent in the request body, which will be serialized.
+ * @param builder A configuration block for the [HttpRequestBuilder], allowing custom headers or settings.
+ */
+suspend inline fun <reified Request, reified Response : Any> HttpClient.put(
+    route: String,
+    queryParams: Map<String, Any> = mapOf(),
+    body: Request,
+    crossinline builder: HttpRequestBuilder.() -> Unit = {}
+): Result<Response, DataError.Remote> {
+    return safeCall {
+        put {
+            url(constructRoute(route))
+            queryParams.forEach { (key, value) ->
+                parameter(key, value)
+            }
+            setBody(body)
+            builder()
+        }
+    }
+}
+
+/**
+ * Performs a DELETE request with the specified [route] and [queryParams],
+ * returning a [Result] to simplify error handling and response parsing.
+ *
+ * @param route The relative or absolute URL for the request.
+ * @param queryParams A map of key-value pairs to be appended to the URL.
+ * @param builder A configuration block for the [HttpRequestBuilder], allowing custom headers or settings.
+ */
+suspend inline fun <reified Request, reified Response : Any> HttpClient.delete(
+    route: String,
+    queryParams: Map<String, Any> = mapOf(),
+    crossinline builder: HttpRequestBuilder.() -> Unit = {}
+): Result<Response, DataError.Remote> {
+    return safeCall {
+        delete {
+            url(constructRoute(route))
+            queryParams.forEach { (key, value) ->
+                parameter(key, value)
+            }
+            builder()
+        }
     }
 }
 
@@ -45,14 +127,15 @@ suspend inline fun <reified T> safeCall(
  * @return A [Result] containing either the success value of type [T] or a [DataError.Remote].
  */
 suspend inline fun <reified T> responseToResult(response: HttpResponse): Result<T, DataError.Remote> {
-    return when(response.status.value) {
-        in 200 .. 299 -> {
+    return when (response.status.value) {
+        in 200..299 -> {
             try {
                 Result.Success(response.body<T>())
-            } catch(_: NoTransformationFoundException) {
+            } catch (_: NoTransformationFoundException) {
                 Result.Failure(DataError.Remote.SERIALIZATION)
             }
         }
+
         400 -> Result.Failure(DataError.Remote.BAD_REQUEST)
         401 -> Result.Failure(DataError.Remote.UNAUTHORIZED)
         403 -> Result.Failure(DataError.Remote.FORBIDDEN)
@@ -75,5 +158,34 @@ fun constructRoute(route: String): String {
         route.contains(UrlConstants.BASE_URL_HTTP) -> route
         route.startsWith("/") -> "${UrlConstants.BASE_URL_HTTP}$route"
         else -> "${UrlConstants.BASE_URL_HTTP}/$route"
+    }
+}
+
+/**
+ * Executes a network call in a platform-safe way, handling platform-specific network exceptions
+ * and converting them into [DataError.Remote] failures.
+ *
+ * @param execute A suspending lambda that performs the actual network request and returns an [HttpResponse].
+ * @param handleResponse A suspending lambda that processes the successful [HttpResponse] and maps it to a [Result].
+ * @return A [Result] containing either the success value of type [T] or a [DataError.Remote].
+ */
+expect suspend fun <T> platformSafeCall(
+    execute: suspend () -> HttpResponse,
+    handleResponse: suspend (HttpResponse) -> Result<T, DataError.Remote>
+): Result<T, DataError.Remote>
+
+/**
+ * A convenience wrapper around [platformSafeCall] that uses [responseToResult] to handle the response.
+ *
+ * @param execute A suspending lambda that performs the actual network request and returns an [HttpResponse].
+ * @return A [Result] containing either the success value of type [T] or a [DataError.Remote].
+ */
+suspend inline fun <reified T> safeCall(
+    noinline execute: suspend () -> HttpResponse
+): Result<T, DataError.Remote> {
+    return platformSafeCall(
+        execute = execute
+    ) { response ->
+        responseToResult(response)
     }
 }
