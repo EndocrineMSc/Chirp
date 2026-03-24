955c3520c930b8acd044aa96614aefb4d10d1cda
Sebastian Schulz <s.schulz@oput.de>
2026-03-24 20:24:10 +0100

    feat: Add adaptive dialog sheet layout

---
+++ core/designsystem/src/commonMain/kotlin/com/endocrine/core/designsystem/components/dialogs/ChirpAdaptiveDialogSheetLayout.kt
@@ -0,0 +1,24 @@
+package com.endocrine.core.designsystem.components.dialogs
+
+import androidx.compose.runtime.Composable
+import com.endocrine.core.presentation.util.currentDeviceConfiguration
+
+@Composable
+fun ChirpAdaptiveDialogSheetLayout(
+    onDismiss: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    val configuration = currentDeviceConfiguration()
+
+    if (configuration.isMobile) {
+        ChirpBottomSheet(
+            onDismiss = onDismiss,
+            content = content
+        )
+    } else {
+        ChirpDialogContent(
+            onDismiss = onDismiss,
+            content = content
+        )
+    }
+}
--- core/presentation/src/commonMain/kotlin/com/endocrine/core/presentation/util/DeviceConfiguration.kt
+++ core/presentation/src/commonMain/kotlin/com/endocrine/core/presentation/util/DeviceConfiguration.kt
@@ -27,6 +27,12 @@ enum class DeviceConfiguration {
     TABLET_LANDSCAPE,
     DESKTOP;
 
+    val isMobile: Boolean
+        get() = this in listOf(
+            MOBILE_PORTRAIT,
+            MOBILE_LANDSCAPE
+        )
+
     companion object {
         /**
          * Maps a [WindowSizeClass] to a [DeviceConfiguration] based on width and height thresholds.
@@ -36,12 +42,16 @@ enum class DeviceConfiguration {
                 when {
                     minWidthDp < WIDTH_DP_MEDIUM_LOWER_BOUND &&
                             minHeightDp >= HEIGHT_DP_MEDIUM_LOWER_BOUND -> MOBILE_PORTRAIT
+
                     minWidthDp >= WIDTH_DP_EXPANDED_LOWER_BOUND &&
                             minHeightDp < HEIGHT_DP_MEDIUM_LOWER_BOUND -> MOBILE_LANDSCAPE
+
                     minWidthDp in WIDTH_DP_MEDIUM_LOWER_BOUND..WIDTH_DP_EXPANDED_LOWER_BOUND &&
                             minHeightDp >= HEIGHT_DP_EXPANDED_LOWER_BOUND -> TABLET_PORTRAIT
+
                     minWidthDp >= WIDTH_DP_EXPANDED_LOWER_BOUND &&
                             minHeightDp in HEIGHT_DP_MEDIUM_LOWER_BOUND..HEIGHT_DP_EXPANDED_LOWER_BOUND -> TABLET_LANDSCAPE
+
                     else -> DESKTOP
                 }
             }
